d2d236103be2544169f3d64edfc1ea1b1633e41b
Ethan <ehenley2@cub.uca.edu>
2026-09-23 14:32:27 -0500

    Add project files.

---
+++ CompSci_Lab_4_EthanHenley/Lab_4_Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+
+
+using namespace std;
+
+int main() {
+	string foodName;
+	int quantity;
+	double price;
+
+	cout << "Enter food name: ";
+	getline(cin, foodName);
+
+	cout << "Enter quantity: ";
+	cin >> quantity;
+
+	cout << "Enter price per item: ";
+	cin >> price;
+
+	double total = quantity * price;
+
+	cout << "/n--- RECIEPT ---" << endl;
+	cout << "Item: " << foodName << endl;
+	cout << "Quantity: " << quantity << endl;
+	cout << "Total Price: $" << total << endl;
+
+	return 0;
+}
